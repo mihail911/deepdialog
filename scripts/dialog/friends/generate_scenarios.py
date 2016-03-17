c3814ff83061a109bd938803b60643de85208ef8
Anusha Balakrishnan <anusha.bala28@gmail.com>
2016-03-16 22:06:44 -0700

    script to generate scenarios for friend task

--- scripts/dialog/friends/generate_scenarios.py
+++ scripts/dialog/friends/generate_scenarios.py
@@ -1,7 +1,5 @@
 __author__ = 'anushabala'
-import re
 
-date_range_pattern = r'([0-9]{4})-([0-9]{}4)'
 import numpy as np
 from argparse import ArgumentParser
 import json
@@ -10,19 +8,12 @@ import os
 
 
 class Friend(object):
-    def __init__(self, name, major, school, school_start, school_end, company=None, company_start=None,
-                 company_end=None):
+    def __init__(self, name, major, school, company):
         self.name = name
         self.school = {'major': major,
-                       'name': school,
-                       'start': school_start,
-                       'end': school_end}
-        if company and company_start and company_end:
-            self.company = {'name': company,
-                            'start': company_start,
-                            'end': company_end}
-        else:
-            self.company = None
+                       'name': school
+                       }
+        self.company = {'name': company}
 
     def __info__(self):
         return {
@@ -41,24 +32,6 @@ class Friend(object):
         if not self.company or not other_friend.company:
             return False
         return self.company["name"] == other_friend.company["name"]
-
-    def school_overlap(self, other_friend):
-        if not self.same_school(other_friend):
-            return False
-        # my_range = xrange(self.school["start"], self.school["end"]+1)
-        other_range = xrange(other_friend.school["start"], other_friend.school["end"] + 1)
-        if self.school["start"] in other_range or self.school["end"] in other_range:
-            return True
-
-    def company_overlap(self, other_friend):
-        if not self.company or not other_friend.company:
-            return False
-        if not self.same_company(other_friend):
-            return False
-        # my_range = xrange(self.company["start"], self.company["end"]+1)
-        other_range = xrange(other_friend.company["start"], other_friend.company["end"] + 1)
-        if self.company["start"] in other_range or self.company["end"] in other_range:
-            return True
 
 
 class FriendNetwork(object):
@@ -80,9 +53,9 @@ class FriendNetwork(object):
         potential_friends = []
         for other_friend in self.friends:
             if other_friend.name != friend.name:
-                if (friend.same_major_and_school(other_friend) or friend.same_school(other_friend)) and friend.school_overlap(other_friend):
+                if friend.same_major_and_school(other_friend):
                     potential_friends.append(other_friend)
-                elif friend.same_company(other_friend) and friend.company_overlap(other_friend):
+                elif friend.same_company(other_friend):
                     potential_friends.append(other_friend)
         return potential_friends
 
@@ -94,7 +67,8 @@ class FriendNetwork(object):
             my_friends = self.relationships[person]
             for friend in my_friends:
                 other_friends = self.relationships[friend]
-                connections = [f for f in other_friends if f not in my_friends and f not in added_friends]
+                connections = [f for f in other_friends if f not in my_friends and f not in added_friends
+                               and not f.same_school(person) and not f.same_company(person)]
                 second_degree.extend([(person, connection, friend) for connection in connections])
 
         return second_degree
@@ -116,19 +90,13 @@ class NetworkGenerator(object):
     same_school = 0.4
     same_major_and_school = 0.5
     same_company = 0.5
-    p_threshold = 0.2
+    p_threshold = 0.3
 
     def generate_random_friend(self, name):
         major = np.random.choice(self.majors)
         school = np.random.choice(self.schools)
-        school_start = np.random.choice(xrange(self.min_year, self.current_year + 1))
-        school_end = min(school_start + np.random.choice(self.school_duration), self.current_year)
-        if school_end < self.current_year:
-            company = np.random.choice(self.companies)
-            company_start = np.random.choice(xrange(school_end, self.current_year))
-            company_end = min(company_start + np.random.choice(self.company_duration), self.current_year)
-            return Friend(name, major, school, school_start, school_end, company, company_start, company_end)
-        return Friend(name, major, school, school_start, school_end)
+        company = np.random.choice(self.companies)
+        return Friend(name, major, school, company)
 
     def __init__(self, N=50, names_file='data/person_names.txt'):
         self.network_size = N
@@ -178,10 +146,9 @@ class ScenarioGenerator(object):
         connection = connected_users[2]
         user1_friends = [connection]
         user2_friends = [connection]
+        scenario["connection"] = {"info": connection.__info__()}
         scenario["user1"] = {"info": user1.__info__()}
         scenario["user2"] = {"info": user2.__info__()}
-
-
 
         # add all friends of each user except their mutual friends (apart from the one common connection
         user1_friends.extend([f for f in self.network.relationships[user1] if f not in self.network.relationships[user2]])
@@ -199,7 +166,6 @@ class ScenarioGenerator(object):
                 if friend not in user2_friends and np.random.uniform() <= 0.5:
                     user1_friends.append(friend)
             ctr += 1
-
 
         ctr = 0
         while ctr < len(self.network.friends) and \
@@ -233,20 +199,10 @@ def write_user(info, outfile, fewer_lines=False):
     else:
         outfile.write("\n")
     outfile.write("\tMajor: %s" % info["school"]["major"])
-    if fewer_lines:
-        outfile.write("\t")
-    else:
-        outfile.write("\n")
-    outfile.write("\tSchool dates: %d - %d" % (info["school"]["start"], info["school"]["end"]))
     outfile.write("\n")
-    if info["company"]:
-        outfile.write("\tCompany: %s" % info["company"]["name"])
-        if fewer_lines:
-            outfile.write("\t")
-        else:
-            outfile.write("\n")
-        outfile.write("\tCompany dates: %d - %d\n" % (info["company"]["start"], info["company"]["end"]))
-    
+    outfile.write("\tCompany: %s" % info["company"]["name"])
+    outfile.write("\n\n")
+
     
 def write_scenario_to_readable_file(scenario, user1_file, user2_file):
     write_user(scenario["user1"]["info"], user1_file)
@@ -261,6 +217,10 @@ def write_scenario_to_readable_file(scenario, user1_file, user2_file):
         user2_file.write("\n")
 
 
+def write_scenarios_to_json(scenarios, json_file):
+    json.dump(scenarios, open(json_file, 'w'))
+
+
 def write_json_to_file(network, outfile):
     json_network = json.dumps(network.__info__())
     outfile.write(json_network+"\n")
@@ -270,19 +230,21 @@ def main(args):
     outfile = open(args.output, 'w')
 
     num_scenarios = args.num_scenarios
-    num_networks = args.num_networks
     generator = NetworkGenerator(args.size)
     generator.create_network()
+    scenarios = []
     for i in xrange(0, num_scenarios):
         scen_file_1 = open(os.path.join(args.scenario_dir, 'scenario%d_User1.out' % i,), 'w')
         scen_file_2 = open(os.path.join(args.scenario_dir, 'scenario%d_User2.out' % i,), 'w')
         scenario_gen = ScenarioGenerator(generator.network)
         scenario = scenario_gen.generate_scenario(num_friends=10)
+        scenarios.append(scenario)
         write_scenario_to_readable_file(scenario, scen_file_1, scen_file_2)
-        write_json_to_file(generator.network, outfile)
+        # write_json_to_file(generator.network, args.output)
         scen_file_1.close()
         scen_file_2.close()
 
+    write_scenarios_to_json(scenarios, args.output)
     outfile.close()
 
 
@@ -290,8 +252,8 @@ if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument('--size', type=int, default=50, help='Size of network to generate')
     parser.add_argument('--output', type=str, default='data/scenarios.json', help='File to write networks to.')
+    parser.add_argument('--n')
     parser.add_argument('--num_scenarios', type=int, default=100, help='Number of scenarios to generate')
-    parser.add_argument('--num_networks', type=int, default=1, help='Number of networks to generate')
     parser.add_argument('--scenario_dir', default='data/scenarios', help='File to write scenario to')
 
     clargs = parser.parse_args()
