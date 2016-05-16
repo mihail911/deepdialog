cbd4e21fb28ac006d385f36ac91f821ca9f61497
Mihail Eric <mihail911@gmail.com>
2016-05-16 12:12:54 -0700

    changed chat.py to accommodate wsgi format

--- chat/chat.py
+++ chat/chat.py
@@ -27,34 +27,84 @@ def clear_data(logging_dir):
         shutil.rmtree(logging_dir)
     os.makedirs(logging_dir)
 
-if __name__ == '__main__':
-    parser = ArgumentParser()
-    parser.add_argument('-p', help="File containing app configuration params", type=str,
-                        default="params.json")
-    parser.add_argument('--host', help="Host IP address to run app on - defaults to localhost", type=str, default="0.0.0.0")
-    parser.add_argument('--log', help="File to log app output to", type=str, default="chat.log")
-    args = parser.parse_args()
-    params_file = args.p
 
-    with open(params_file) as fin:
-        params = json.load(fin)
+def get_params_json():
+    return """{
+    "status_params": {
+	"waiting": {
+	    "num_seconds": 0.5
+	},
 
-    if os.path.exists(params["db"]["location"]):
-        os.remove(params["db"]["location"])
+	"chat": {
+	    "num_seconds": 300,
+	    "presentation_config": {
+		"numerical_preferences": false,
+		"numerical_prices": false
+    	    }
+	},
 
-    init_database(params["db"]["location"])
-    clear_data(params["logging"]["chat_dir"])
-    templates_dir = params["templates_dir"]
-    app = create_app(debug=True, templates_dir=templates_dir)
+	"single_task": {
+	    "num_seconds": -1,
+	    "max_tasks": 3
+	},
 
-    with open(params["scenarios_json_file"]) as fin:
-        scenarios = json.load(fin)
-        scenarios_dict = {v["uuid"]:v for v in scenarios}
+	"finished": {
+	    "num_seconds": 15
+	}
+    },
 
-    app.config["user_params"] = params
-    app.config["scenarios"] = scenarios_dict
-    app.config["outcomes"] = defaultdict(lambda : -1)
+    "scenarios_json_file": "friend_scenarios.json",
+    "single_tasks_json_file": "single_tasks.json",
+    "connection_timeout_num_seconds": 45,
+  	"templates_dir": "dialogue_templates",
 
-    # logging.basicConfig(filename=params["logging"]["app_logs"], level=logging.INFO)
+    "db": {
+	"location": "/Users/mihaileric/Documents/Research/Ford Project/DataCollection/chat_state.db"
+    },
 
-    socketio.run(app, host=args.host)
+    "logging": {
+	"app_logs": "/Users/mihaileric/Documents/Research/Ford Project/deepdialog/chat/chat.log",
+	"chat_dir": "/Users/mihaileric/Documents/Research/Ford Project/deepdialog/chat/transcripts",
+	"chat_delimiter": "---"
+    }
+    }"""
+
+
+#if __name__ == '__main__':
+
+# For not using the config file to get params
+params = get_params_json()
+host = "127.0.0.1"
+#log =
+
+#parser = ArgumentParser()
+#parser.add_argument('-p', help="File containing app configuration params", type=str,
+#                    default="params.json")
+#parser.add_argument('--host', help="Host IP address to run app on - defaults to localhost", type=str, default="0.0.0.0")
+#parser.add_argument('--log', help="File to log app output to", type=str, default="chat.log")
+#args = parser.parse_args()
+#params_file = args.p
+
+#with open(params_file) as fin:
+#    params = json.load(fin)
+
+if os.path.exists(params["db"]["location"]):
+    os.remove(params["db"]["location"])
+
+init_database(params["db"]["location"])
+clear_data(params["logging"]["chat_dir"])
+templates_dir = params["templates_dir"]
+app = create_app(debug=True, templates_dir=templates_dir)
+
+with open(params["scenarios_json_file"]) as fin:
+    scenarios = json.load(fin)
+    scenarios_dict = {v["uuid"]:v for v in scenarios}
+
+app.config["user_params"] = params
+app.config["scenarios"] = scenarios_dict
+app.config["outcomes"] = defaultdict(lambda: -1)
+
+# logging.basicConfig(filename=params["logging"]["app_logs"], level=logging.INFO)
+
+# May have to remove this for now -- is this necessary to have?
+socketio.run(app, host=host)
